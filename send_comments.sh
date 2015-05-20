0a3441c75c0b623d5e06f4367495187f54b5df90
Shane Stecker <nutallergist@gmail.com>
2015-05-20 12:27:23 -0400

    +24h seems to be more what i want

--- send_comments.sh
+++ send_comments.sh
@@ -5,7 +5,7 @@ SELF="$0" ; test "X${SELF}" = "X" && exit 2
 DIR=`dirname ${SELF}`
 BASE=`basename ${SELF}`
 cd ${DIR}/ || exit 9
-RDY2SND=`find MESGS -type f -mtime +1`
+RDY2SND=`find MESGS -type f -mtime +24h`
 test "X${RDY2SND}" = "X" && exit 10
 NEWDONES=`cat MESGS/*|grep -v -i merge|sort -u|grep '.'`
 test "X${NEWDONES}" = "X" && exit 10
