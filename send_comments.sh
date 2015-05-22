a27309519fcfcdc9901f44be3e1b9fbf3c9f4301
Shane Stecker <nutallergist@gmail.com>
2015-05-22 11:03:18 -0400

    better validation filter on NEWDONES, and stash sent dones

--- send_comments.sh
+++ send_comments.sh
@@ -5,14 +5,29 @@ SELF="$0" ; test "X${SELF}" = "X" && exit 2
 DIR=`dirname ${SELF}`
 BASE=`basename ${SELF}`
 cd ${DIR}/ || exit 9
+export CWD=`pwd`
 RDY2SND=`find MESGS -type f -mtime +24h`
 ##RDY2SND=`find MESGS -type f`
 test "X${RDY2SND}" = "X" && exit 10
-NEWDONES=`cat MESGS/*|grep -v -i merge|sort -u|grep '.'`
+NEWDONES=`cat MESGS/*|grep -v -i merge|sort -u|grep "[A-Z,a-z,0-9]"`
 test "X${NEWDONES}" = "X" && exit 10
+mkdir -p ./logs.git
+test -d ./logs.git/.git || git init logs.git
+cat repos/.did/.git/config > ./logs.git/.git/config
 
 echo "${NEWDONES}"|while read line ; do
-  ./didit.rb "${line}" && rm ./MESGS/*
+  ./didit.rb "${line}" && {
+    perl -p -i -e "s|^${line}\n||g"  MESGS/*
+    echo "${line}" > logs.git/DIDS
+    cd logs.git || exit 19
+    git add DIDS
+    git commit -m "${line}" DIDS
+    cd "${CWD}" || exit 23
+    }
   done
 
+cd "${CWD}" || exit 29
+EMPTYFILES=`grep  -L  "[A-Z,a-z,0-9]" MESGS/*`
+
+test "X${EMPTYFILES}" != "X" && rm -f ${EMPTYFILES}
 
