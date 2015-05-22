78174b6bba9998b5f55f5e44a5f3de887f301da4
Shane Stecker <nutallergist@gmail.com>
2015-05-22 14:01:50 -0400

    now send_comments.sh logs what it sends, and filter what has been sent out of what it posts to avoid dups within the current day, by stashing posts into logs.git

--- send_comments.sh
+++ send_comments.sh
@@ -6,25 +6,37 @@ DIR=`dirname ${SELF}`
 BASE=`basename ${SELF}`
 cd ${DIR}/ || exit 9
 export CWD=`pwd`
-RDY2SND=`find MESGS -type f -mtime +24h`
-##RDY2SND=`find MESGS -type f`
+##RDY2SND=`find MESGS -type f -mtime +24h`
+RDY2SND=`find MESGS -type f`
 test "X${RDY2SND}" = "X" && exit 10
-NEWDONES=`cat MESGS/*|grep -v -i merge|sort -u|grep "[A-Z,a-z,0-9]"`
-test "X${NEWDONES}" = "X" && exit 10
+####NEWDONES=`cat MESGS/*|grep -v -i merge|sort -u|grep "[A-Z,a-z,0-9]"`
+####test "X${NEWDONES}" = "X" && exit 10
 mkdir -p ./logs.git
 test -d ./logs.git/.git || git init logs.git
 cat repos/.did/.git/config > ./logs.git/.git/config
-
-echo "${NEWDONES}"|while read line ; do
-  ./didit.rb "${line}" && {
+ALREADYSENTTODAY=`git --git-dir=logs.git/.git log --format=%s  --author=sstecker  --since=12am`
+test "X${ALREADYSENTTODAY}"  != "X" && {
+  echo "${ALREADYSENTTODAY}"|while read line ; do
     perl -p -i -e "s|^${line}\n||g"  MESGS/*
-    echo "${line}" > logs.git/DIDS
-    cd logs.git || exit 19
-    git add DIDS
-    git commit -m "${line}" DIDS
-    cd "${CWD}" || exit 23
-    }
-  done
+    done
+  }
+
+  
+NEWDONES=`cat MESGS/*|grep -v -i merge|sort -u|grep "[A-Z,a-z,0-9]"`
+
+
+test "X${NEWDONES}" != "X" &&  {
+  echo "${NEWDONES}"|while read line ; do
+    ./didit.rb "${line}" && {
+      perl -p -i -e "s|^${line}\n||g"  MESGS/*
+      echo "${line}" > logs.git/DIDS
+      cd logs.git || exit 19
+      git add DIDS
+      git commit -m "${line}" DIDS
+      cd "${CWD}" || exit 23
+      }
+    done
+  }
 
 cd "${CWD}" || exit 29
 EMPTYFILES=`grep  -L  "[A-Z,a-z,0-9]" MESGS/*`
